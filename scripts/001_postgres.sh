40d2856104a39bb42db30571950b8a7f12f8d795
phil <philip.main@renalregistry.nhs.uk>
2025-05-28 14:28:19 +0100

    .

--- scripts/001_postgres.sh
+++ scripts/001_postgres.sh
@@ -5,13 +5,13 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB"  -c 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -c "ALTER USER "$POSTGRES_USER" WITH SUPERUSER;"
 
 
-# initialise the modality codes table from csv
+# initialise the modality codes table 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB"  -f tables/modality_codes/schema.sql
 
 # copy data 
-#psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<EOF
-#\copy extract.modality_codes FROM 'tables/modality_codes/v4.csv' WITH (FORMAT csv)
-#EOF
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<EOF
+\copy extract.modality_codes FROM 'tables/modality_codes/v4.csv' WITH (FORMAT csv)
+EOF
 
 # dump data
-pg_dump --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" --schema=extract --clean --if-exists --no-owner --no-privileges > /output/registry_codes_dump.sql
+pg_dump --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" --schema=extract --clean --if-exists --no-owner --no-privileges > /output/registry_codes.dump
