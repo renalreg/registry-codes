5a90b76241978a04d722628a90b5778d4e13a925
phil <philip.main@renalregistry.nhs.uk>
2025-12-15 08:30:18 +0000

    updated actions to fix failed update

---
+++ scripts/load_into_remote.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from registry_codes.utils import load_data
+
+TABLE_NAME = "modality_codes"
+URL = "*****"
+engine = create_engine(URL)
+
+
+try: 
+    inserted_rows = load_data(TABLE_NAME, engine)
+    print(f"Inserted {inserted_rows} rows into {TABLE_NAME}")
+except Exception as e:
+    print(f"Error loading data into {TABLE_NAME}: {e}")
