ca7da76f4eac635f2ad2d16de7fe8208a96dde04
phil <philip.main@renalregistry.nhs.uk>
2026-02-25 11:06:25 +0000

    added complete set or rr1 -> region mapping

--- scripts/load_into_remote.py
+++ scripts/load_into_remote.py
@@ -1,15 +1,23 @@
 """
-Utility script which is useful for doing one of table syncs to an arbitary
-table.
+Small utility script to load data in an ad-hoc way by specifying a table and a
+db-url.
 """
 
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
+
 from registry_codes.utils import load_data
 
-TABLE_NAME = "modality_codes"
-URL = "*****"
-engine = create_engine(URL)
+load_dotenv()
+URL = os.getenv("URL")
+TABLE_NAME = "code_map"
 
+if not URL:
+    raise RuntimeError("Missing required environment variable: URL")
+
+engine = create_engine(URL)
 
 try:
     inserted_rows = load_data(TABLE_NAME, engine)
