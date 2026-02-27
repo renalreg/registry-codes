b6b6845e7fca47df5302657882a88aed23adefff
phil <philip.main@renalregistry.nhs.uk>
2026-02-27 14:18:30 +0000

    add facility to region mappings and RAJ missing satellites to facility table

--- scripts/load_into_remote.py
+++ scripts/load_into_remote.py
@@ -1,6 +1,7 @@
 """
-Small utility script to load data in an ad-hoc way by specifying a table and a
-db-url.
+Small utility script to load data into a table in an ad-hoc way by specifying a table and a
+db-url. Care should be taken in using this script as it failing can lead to all
+the data in a table being dropped.
 """
 
 import os
@@ -12,7 +13,7 @@ from registry_codes.utils import load_data
 
 load_dotenv()
 URL = os.getenv("URL")
-TABLE_NAME = "code_map"
+TABLE_NAME = "facility_new"
 
 if not URL:
     raise RuntimeError("Missing required environment variable: URL")
